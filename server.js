7b809569a128492e5d3adb118c10cc0d8771fbd5
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-04-10 02:43:08 -0400

    Attempted code to generate a random id

--- server.js
+++ server.js
@@ -8,22 +8,26 @@ const app = express()
 
 const PORT = 3001;
 
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
 
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+});
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, `./public/index.html`))
-})
+});
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    console.info(`${req.method} request received`)
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err)
         } else {
-            let parsedNotes = JSON.parse(data)
+            let parsedNotes = JSON.stringify(data)
             res.json(parsedNotes)
+            console.log('Im here')
         }
     })
 })
@@ -35,7 +39,7 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuid()
+            note_id: uuid(),
         };
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
@@ -50,9 +54,19 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.error('Error in adding new note')
     }
-    // fs.appendFile('db.json', res.json(`${req}`), (error) =>{
-    //     error ? console.error(error) : console.log('success')
-    // })
+})
+app.get('/api/notes/:note_id', (req, res)=>{
+    if (req.params.note_id){
+        const noteId = req.params.note_id;
+        console.info(`${req.method} received`)
+        for (let i = 0; i <db.length; i++){
+            const currentNote = db[i];
+            if (currentNote.review_id === noteId){
+                res.json(currentNote);
+                return;
+            }
+        }
+    }
 })
 
 app.listen(PORT, () => {
