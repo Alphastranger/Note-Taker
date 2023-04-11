596a90cd160887c21456d182d167f0402cbbe56b
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-04-10 21:40:03 -0400

    Fixed the error with the GET requests

--- server.js
+++ server.js
@@ -2,30 +2,29 @@ const express = require('express');
 const fs = require('fs')
 const path = require('path')
 const db = require('./db/db.json')
-const uuid = require('uuid')
+const {v4: uuidv4} = require('uuid')
 
 const app = express()
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
 
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, `./public/index.html`))
-});
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, `./public/index.html`))
+// });
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received`)
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err)
         } else {
-            let parsedNotes = JSON.stringify(data)
+            let parsedNotes = JSON.parse(data)
             res.json(parsedNotes)
             console.log('Im here')
         }
@@ -39,7 +38,7 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            note_id: uuid(),
+            note_id: uuidv4(),
         };
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
@@ -68,7 +67,9 @@ app.get('/api/notes/:note_id', (req, res)=>{
         }
     }
 })
-
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, `./public/index.html`))
+});
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
