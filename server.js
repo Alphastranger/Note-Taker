3ef5a66e939b9b93dcef31509858d3e38a51b117
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-04-10 22:41:00 -0400

    Some additional work done on the unique id

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ app.use(express.static('public'))
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, `./public/index.html`))
-// });
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received`)
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -40,6 +37,7 @@ app.post('/api/notes', (req, res) => {
             text,
             note_id: uuidv4(),
         };
+        console.log(newNote.note_id)
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.error(err);
@@ -54,15 +52,31 @@ app.post('/api/notes', (req, res) => {
         res.error('Error in adding new note')
     }
 })
-app.get('/api/notes/:note_id', (req, res)=>{
+app.get('api/notes/:note_id', (req, res)=>{
+    console.log(req.params.note_id)
+    // const noteID = req.params.note_id;
+    // fs.readFile('./db/db.json', 'utf8', (err, data)=>{
+    //     if (err) {
+    //         console.error(err);
+    //     } else {
+    //         console.log(data)
+    //         for (let i = 0; i < data.length; i++){
+    //             const currentNote = data[i];
+    //             if (currentNote.note_id === noteID){
+    //                 console.log('So it is')
+    //                 res.json(currentNote);
+    //             }
+    //         }
+    //     }
+    // })
     if (req.params.note_id){
         const noteId = req.params.note_id;
         console.info(`${req.method} received`)
         for (let i = 0; i <db.length; i++){
             const currentNote = db[i];
-            if (currentNote.review_id === noteId){
+            if (currentNote.note_id === noteId){
+                console.log(currentNote)
                 res.json(currentNote);
-                return;
             }
         }
     }
